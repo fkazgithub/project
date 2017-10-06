ed9bfedeb30ae8430940ed7314c21a7c91685176
府川和樹 <kazukifukawa1012@gmail.com>
2017-10-07 00:52:47 +0900

    view edit

--- payjp_app/app/controllers/products_controller.rb
+++ payjp_app/app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
 end
+
+def purchase
+   Payjp.api_key = PAYJP_SECRET_KEY
+   Payjp::Charge.create(
+      :amount => 3500,
+      :card => params['payjp-token'],
+      :currency => 'jpy'
+    )
+end
--- payjp_app/config/initializers/setting.rb
+++ payjp_app/config/initializers/setting.rb
@@ -1,4 +1,4 @@
 # テスト環境用公開可能鍵
-PAYJP_PUBLIC_KEY = '上記ページで確認できる「公開鍵」'
+PAYJP_PUBLIC_KEY = 'sk_test_5934a3091f28f8c621d0809c'
 # テスト環境用非公開鍵
-PAYJP_SECRET_KEY = '上記ページで確認できる「秘密鍵」'
+PAYJP_SECRET_KEY = 'pk_test_099dcf13efc6be2222818c38'
